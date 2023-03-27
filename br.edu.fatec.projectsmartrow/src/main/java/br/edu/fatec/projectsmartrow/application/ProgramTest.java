7f0f2f717ae40aa1b97a6cd4a6c6a80bd6aa9c9a
Richard Guedes <richard.silva46@fatec.sp.gov.br>
2023-03-27 16:05:43 -0300

    Acerto de Bug ao listar todos os Estabelecimentos

--- br.edu.fatec.projectsmartrow/src/main/java/br/edu/fatec/projectsmartrow/application/ProgramTest.java
+++ br.edu.fatec.projectsmartrow/src/main/java/br/edu/fatec/projectsmartrow/application/ProgramTest.java
@@ -23,75 +23,16 @@ public class ProgramTest {
 		EstabelecimentoDAO dao = new EstabelecimentoDAO();
 		CardapioDAO cdao = new CardapioDAO();
 		MesasDAO mdao = new MesasDAO();
-//		
-//		Endereco endereco = new Endereco("cep", "log", "num", "comple", "ref", "bairro", "dsadsaloc", "sp", "paisdd");
-//		endereco.setId(10);
-//		Estabelecimento estabelecimento = new Estabelecimento("nometeste", "cnpjaaaa", "tel1", "tel2", "email1","horario", 1, "imagemaqui", endereco, null);
-//		dao.insertEstabelecimento(estabelecimento);
-//		List<Pratos> pratos = new ArrayList<>();
-//		Pratos p1 = new Pratos(null, "nome prato", "almoco", "cebola", 120.00, "iamgem", null );
-//		Pratos p2 = new Pratos(null, "nome prato2", "almoco", "cebola", 120.00, "iamgem", null );
-//		pratos.add(p1);
-//		pratos.add(p2);
-//		
-//		List<Bebidas> bebidas = new ArrayList<>();
-//		Bebidas b1 = new Bebidas(null, "nome bebida", "refri", 120.00, "iamgem");		
-//		Bebidas b2 = new Bebidas(null, "nome bebida2", "refri", 120.00, "iamgem");
-//		bebidas.add(b1);
-//		bebidas.add(b2);
-//		Cardapio cardapio = new Cardapio(null, pratos, bebidas);
-//		estabelecimento.setCardapio(cardapio);
-//		cdao.insertCardapio(cardapio, estabelecimento);
-//		
-//		Mesas m1 = new Mesas(10, 20);
-//		Mesas m2 = new Mesas(10, 20);
-//		List<Mesas> mesas = new ArrayList();
-//		mesas.add(m1);
-//		mesas.add(m2);
-//		estabelecimento.setMesas(mesas);
-//		mdao.insertMesas(estabelecimento);
-//	
-//		Mesas mesa = new Mesas();
-//		ps = conn.prepareStatement("SELECT * FROM MESAS WHERE ID_ESTABELECIMENTO = 17");
-//		
-//		ResultSet rs = ps.executeQuery();
-//		List<Mesas> list = new ArrayList<>(); 
-//		list = mesa.buscarMesaPorEstabelecimento(23);
-//		mesa.imprimirMesas(list);
-//		List<Pratos> list = new ArrayList<>();
-//		Pratos pratos = new Pratos();
-//		list = pratos.buscarPratosPorCardapio(14);
-//		pratos.imprimirPratos(list);
 		
-//		Cardapio cardapio = new Cardapio();
-//		cardapio = cardapio.buscarCardapioPorIdEstabelecimento(23);
-//		cardapio.imprimirCardapio();
-//		
-		Estabelecimento e1 = new Estabelecimento();
-		EstabelecimentoDAO edao = new EstabelecimentoDAO();
-		EnderecoService eservice = new EnderecoService();
-		EnderecoDAO enddao = new EnderecoDAO();
-		EstabelecimentoResources er = new EstabelecimentoResources();
-//		
-		e1 = edao.buscarEstabelecimentoPorId(24);
-		e1.imprimirEstabelecimento();
-		e1.getCardapio().imprimirCardapio();
-		System.out.println("===============================");
-		
-//		er.atualizarEstabelecimento(e1);
-//		e1.imprimirEstabelecimento();
+		Estabelecimento est = new Estabelecimento();
+		est = dao.buscarEstabelecimentoPorId(20);
+		est.imprimirEstabelecimento();
+		est.getCardapio().imprimirCardapio();
+		System.out.println("IDCARPDAIO: " + est.getCardapio().getIDCardapio());
 		
 		
 		
-//		EnderecoResource endres = new EnderecoResource();
-//		EnderecoDAO enddao = new EnderecoDAO();
-//		Endereco endereco = new Endereco();
-//		endereco = enddao.buscarEnderecoPorEstabelecimentoId(20);
-//		endereco.exibirEndereco();
-//		System.out.println("ID Endereco : " + endereco.getId());
-//		
-//		endereco = endres.atualizarEndereco(endereco);
-	
+		dao.listarTodosEstabelecimentos();
 		
 		
 		
